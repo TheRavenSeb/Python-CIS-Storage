1e44d72e68570f64dd24540d3917f290f0b67b2f
Caleb Thomas <64808289+TheRavenSeb@users.noreply.github.com>
2025-01-29 18:31:17 -0500

    Chore(TempConverter.py): Added better comments

--- 002/TempConverter.py
+++ 002/TempConverter.py
@@ -8,9 +8,11 @@ startCommand: py TempConverter.py
 
 """
 ## formuala = °F = °C × (9/5) + 32
-C = 4
+C = 4 # Celsius
 
+## Uses formuala to calculate the farinhates 
 F = C * (9/5) + 32
 
+#Print the output into a readable format
 print("%d\u00b0C is %s\u00b0F" %(C,float(F)))
 
